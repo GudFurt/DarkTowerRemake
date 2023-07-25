b54d6d349b994be4845a3a5fa5e12ef0eaab86c9
Sas Alexandru <aexsas@outlook.com>
2023-07-25 22:05:18 +0300

    Added Sprites and Base Movement
    PNG tileset added and split into individual sprites and assets. Basic up down left right movement with no animations coded

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float moveSpeed;
+    public bool isMoving;
+    public Vector2 input;
+
+
+
+
+
+    private void Update()
+    {
+        if(!isMoving){
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if(input.x != 0) input.y = 0;
+
+            if(input != Vector2.zero){
+                var targetPosition = transform.position;
+                targetPosition.x += input.x;
+                targetPosition.y += input.y;
+
+                StartCoroutine(Move(targetPosition));
+            }
+        }
+
+        
+    }
+
+    IEnumerator Move(Vector3 targetPosition){
+        isMoving = true;
+
+        while((targetPosition - transform.position).sqrMagnitude > Mathf.Epsilon){
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPosition;
+        
+        isMoving = false;
+
+    }
+}
